f5dabf12f6a9c75a593ddfb3a68af1aad00e3ec0
StaticESC <slushiboy21@gmail.com>
2020-08-01 14:08:01 +0530

    Added Player Animations and refactored movement code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,49 +11,61 @@ public class Player : MonoBehaviour
 
     //declarations & Cache
     Rigidbody2D myRigidbody;
-    Camera mainCamera;
+    Animator myAnimator;
 
 
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
-        
+        myAnimator =  GetComponentInChildren<Animator>();
     }
 
     
     void Update()
     {
+
         Movement();
         LookAtMouse();
+        //Shoot();
     }
 
     private void LookAtMouse()
     {
+        
         var mainMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.LookAt(mainMousePos);
+        
         var gameObjectPos = transform.position;
-        var directionToLookIn = new Vector2(mainMousePos.x - gameObjectPos.x, mainMousePos.y - gameObjectPos.y);
+        var directionToLookIn = new Vector2(mainMousePos.x - gameObjectPos.x, mainMousePos.y - gameObjectPos.y).normalized;
         transform.up = directionToLookIn;
+        
     }
 
     private void Movement()
     {
-        
         var controlThrowX = Input.GetAxisRaw("Horizontal");
         var controlThrowY = Input.GetAxisRaw("Vertical");
-        float moveSpeedX, moveSpeedY;
-        if (Input.GetKey(KeyCode.LeftShift)) //run
+        bool isMoving = Mathf.Abs(controlThrowX) > 0f || Mathf.Abs(controlThrowY) > 0f;
+        float moveSpeedX = 0f, moveSpeedY = 0f;
+
+        if (!isMoving)
+        {
+            myAnimator.SetTrigger("Idling");
+        }
+
+        else if (Input.GetKey(KeyCode.LeftShift)) //run
         {
             moveSpeedX = controlThrowX * runSpeed;
             moveSpeedY = controlThrowY * runSpeed;
-            //animation
+            myAnimator.SetTrigger("Moving");
         }
 
-        else
+        else 
         {
             moveSpeedX = controlThrowX * walkSpeed;
             moveSpeedY = controlThrowY * walkSpeed;
-            //animation
+            myAnimator.SetTrigger("Moving");
         }
         Vector2 velocity = new Vector2(moveSpeedX, moveSpeedY);
         myRigidbody.velocity = velocity;
