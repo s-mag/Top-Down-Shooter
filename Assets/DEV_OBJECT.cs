3a1eb33897ce69e6055f13713b4131bf1d0ab48b
StaticESC <slushiboy21@gmail.com>
2020-08-05 21:52:59 +0530

    Added Raycast for bullet (Incomplete)
    > Added Raycast detection on bullet for better collision detection. (Incomplete)
    -----------TODOS-----------
    > Learn Raycasting in 3D and 2D
    > Write your own code

---
+++ Assets/DEV_OBJECT.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEV_OBJECT : MonoBehaviour
+{
+    [SerializeField] float timeScale = 1f;
+
+    private void Update()
+    {
+        Time.timeScale = timeScale;
+    }
+}
--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEditor;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
@@ -14,24 +15,63 @@ public class Bullet : MonoBehaviour
 
     //class-member declarations and cache
     Rigidbody2D myRigidbody;
+    Collider2D myCollider;
+    Vector2 prevPos;
+    LayerMask wallLayerMask;
 
     private void Start()
     {
+        wallLayerMask = LayerMask.GetMask("Walls");
         myRigidbody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
     }
-
     private void Update()
     {
+        prevPos = transform.position;
         MoveBullet();
+        var currentPos = new Vector2(transform.position.x, transform.position.y);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(prevPos, currentPos, (prevPos - currentPos).magnitude);
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit2D hit = hits[i];
+            Debug.Log(hit.collider.name);
+
+            if (hit.collider.gameObject.tag == "Wall")
+            {
+                
+                Debug.Log("TOUCHHHHHHHH");
+                //Destroy(gameObject);
+            }
+           
+             
+
+
+            //Debug.Log(hit.collider.gameObject.name);
+        }
     }
+
+
 
     private void MoveBullet()
     {
         myRigidbody.velocity = new Vector2(transform.up.normalized.x * speed, transform.up.normalized.y * speed);
     }
 
+    
+    /*
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(gameObject);
     }
+    
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+    */
 }
